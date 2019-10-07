972680496cd21cd35a9f3c782e7f59ac4185adb4
Jose Gomez-Dans <jgomezdans@gmail.com>
2019-10-07 17:22:34 +0100

    WCM+Oh2004 updates

--- SAR_retrieval_funcs.py
+++ SAR_retrieval_funcs.py
@@ -7,11 +7,60 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize
 import scipy.stats
-from osgeo import gdal, ogr, osr
 
 
-def wcm_jac(A, V1, B, V2, C, sigma_soil, theta=23):
-    """WCM model and jacobian calculations. The main
+def oh2004(mv, ks, theta):
+    """Oh 2004 soil model as a function of volumetric soil moisture,
+    rms height and angle of incidence. 
+    Parameters
+    ----------
+    mv : float, ndarray
+        volumetric soil moisture m3/m3
+    ks : float
+        product of wavenumber and rms height
+        be aware that both need to have the same units
+    theta : float, ndarray
+        incidence angle [rad]
+    
+    Returns
+    -------
+    Backscatter (linear units) in HH, VV and HV polarisations
+
+    """
+    p1 = (2.0 * theta / np.pi) ** (0.35 * mv ** (-0.65))
+    p2 = np.exp(-0.4 * ks ** 1.4)
+    p = 1 - p1 * p2
+    # p = 1 - (2.*theta/np.pi)**(0.35*mv**(-0.65)) * np.exp(-0.4 * ks**1.4)
+    dp_dmv = 0.2275 * p1 * np.log(2.0 * theta / np.pi) * p2 / (mv ** 1.65)
+    dp_dks = 0.56 * p1 * np.exp(-0.4 * ks ** 1.4) * ks ** 0.4
+    q0 = 0.095 * (0.13 + np.sin(1.5 * theta)) ** 1.4
+    q = q0 * (1 - np.exp(-1.3 * ks ** 0.9))
+    dq_dmv = 0.0
+    dq_dks = q0 * (-1.17 * np.exp(-1.3 * ks ** 0.9) / ks ** 0.1)
+    a = 0.11 * mv ** 0.7 * np.cos(theta) ** 2.2
+    da_dmv = 0.7 * 0.11 * np.cos(theta) ** 2.2 / mv ** 0.3
+    da_dks = 0.0
+    b = 1 - np.exp(-0.32 * ks ** 1.8)
+    db_mv = 0.0
+    db_mks = 0.576 * np.exp(-0.32 * ks ** 1.8) * (ks ** 0.8)
+
+    hv = a * b
+    dhv_dmv = da_dmv * b
+    dhv_dks = db_mks * a
+    vv = a * b / q
+    dvv_dmv = (da_dmv * b * q) / (q * q)
+    dvv_dks = (db_mks * a * q + dq_dks * a * b) / (q * q)
+    hh = hv / q * p
+
+    return [
+        [hh, None],
+        [vv, np.array([dvv_dmv, dvv_dks])],
+        [hv, np.array([dhv_dmv, dhv_dks])],
+    ]
+
+
+def wcm(A, V1, B, V2, mv, ks, theta=23, pol="VV"):
+    """WCM model with Oh2004 model. The main
     assumption here is that we only consider first
     order effects. The vegetation backscatter contribution
     is given by `A*V1`, which is often related to scatterer
@@ -19,32 +68,33 @@ def wcm_jac(A, V1, B, V2, C, sigma_soil, theta=23):
     due to the canopy is controlled by `B*V2`, which is
     often related to canopy moisture content (this is polarisation
     and frequency dependent). The soil backscatter is modelled as
-    a linear function of volumetric soil moisture.
-    
-    This function returns the gradient for all parameters (A, B, 
-    V1, V2 and C)."""
+    using an Oh2004 model.
+    """
     mu = np.cos(np.deg2rad(theta))
     tau = np.exp(-2 * B * V2 / mu)
     veg = A * V1 * (1 - tau)
-    soil = tau * sigma_soil + C
+    isel = ["HH", "VV", "HV"].index(pol.upper())
+    (sigma_soil, (dsoil_dmv, dsoil_dks)) = oh2004(mv, ks, np.deg2rad(theta))[
+        isel
+    ]
+    soil = tau * sigma_soil
 
     der_dA = V1 - V1 * tau
     der_dV1 = A - A * tau
     der_dB = (-2 * V2 / mu) * tau * (-A * V1 + sigma_soil)
     der_dV2 = (-2 * B / mu) * tau * (-A * V1 + sigma_soil)
-    der_dC = 1  # CHECK!!!!!!
-    der_dsigmasoil = tau
+    der_dmv = tau * dsoil_dmv
+    der_dks = tau * dsoil_dks
 
-    # Also returns der_dV1 and der_dV2
     return (
         veg + soil,
-        [der_dA, der_dB, der_dC, der_dsigmasoil, der_dV1, der_dV2],
+        np.array([der_dA, der_dV1, der_dB, der_dV2, der_dmv, der_dks]),
     )
 
 
 def cost_obs(x, svh, svv, theta, unc=0.5):
     """Cost function. Order of parameters is
-    A_vv, B_vv, C_vv, A_vh, B_vh, C_vh,
+    A_vv, B_vv, A_vh, B_vh, ks,
     vsm_0, ..., vsm_N,
     LAI_0, ..., LAI_N
     
@@ -54,11 +104,11 @@ def cost_obs(x, svh, svv, theta, unc=0.5):
     although these are just labels!
     """
     n_obs = svh.shape[0]
-    A_vv, B_vv, C_vv, A_vh, B_vh, C_vh = x[:6]
-    vsm = x[6 : (6 + n_obs)]
-    lai = x[(6 + n_obs) :]
-    sigma_vv, dvv = wcm_jac(A_vv, lai, B_vv, lai, C_vv, vsm, theta=theta)
-    sigma_vh, dvh = wcm_jac(A_vh, lai, B_vh, lai, C_vh, vsm, theta=theta)
+    A_vv, B_vv, A_vh, B_vh, ks = x[:5]
+    vsm = x[5 : (5 + n_obs)]
+    lai = x[(5 + n_obs) :]
+    sigma_vv, dvv = wcm(A_vv, lai, B_vv, lai, vsm, ks, pol="VV", theta=theta)
+    sigma_vh, dvh = wcm(A_vh, lai, B_vh, lai, vsm, ks, pol="HV", theta=theta)
     diff_vv = svv - sigma_vv
     diff_vh = svh - sigma_vh
     cost = 0.5 * (diff_vv ** 2 + diff_vh ** 2) / (unc ** 2)
@@ -67,15 +117,14 @@ def cost_obs(x, svh, svv, theta, unc=0.5):
             np.array(
                 [
                     np.sum(dvv[0] * diff_vv),  # A_vv
-                    np.sum(dvv[1] * diff_vv),  # B_vv
-                    np.sum(dvv[2] * diff_vv),  # C_vv
+                    np.sum(dvv[2] * diff_vv),  # B_vv
                     np.sum(dvh[0] * diff_vh),  # A_vh
-                    np.sum(dvh[1] * diff_vh),  # B_vh
-                    np.sum(dvh[2] * diff_vh),
+                    np.sum(dvh[2] * diff_vh),  # B_vh
+                    np.sum(dvv[5] * diff_vv + dvh[5] * diff_vh),  # ks
                 ]
-            ),  # C_vh
-            dvv[3] * diff_vv + dvh[3] * diff_vh,  # vsm
-            (dvv[4] + dvv[5]) * diff_vv + (dvh[4] + dvh[5]) * diff_vh,  # LAI
+            ),
+            dvv[4] * diff_vv + dvh[4] * diff_vh,  # vsm
+            (dvv[1] + dvv[3]) * diff_vv + (dvh[1] + dvh[3]) * diff_vh,  # LAI
         ]
     )
     return cost.sum(), -jac / (unc ** 2)
@@ -94,8 +143,8 @@ def cost_prior(x, svh, svv, theta, prior_mean, prior_unc):
     n_obs = len(svh)
     prior_cost = 0.5 * (prior_mean - x) ** 2 / prior_unc ** 2
     dprior_cost = -(prior_mean - x) / prior_unc ** 2
-    dprior_cost[:6] = 0.0
-    return (prior_cost[6:]).sum(), dprior_cost
+    dprior_cost[:5] = 0.0
+    return (prior_cost[5:]).sum(), dprior_cost
 
 
 def cost_smooth(x, gamma):
@@ -124,152 +173,22 @@ def cost_function(x, svh, svv, theta, gamma, prior_mean, prior_unc, unc=0.8):
     cost2, dcost2 = cost_prior(x, svh, svv, theta, prior_mean, prior_unc)
     # Smooth evolution of LAI
     n_obs = len(svv)
-    lai = x[(6 + n_obs) :]
+    lai = x[(5 + n_obs) :]
     cost3, dcost3 = cost_smooth(lai, gamma)
     tmp = np.zeros_like(dcost1)
-    tmp[(7 + n_obs) : -1] = dcost3
+    tmp[(6 + n_obs) : -1] = dcost3
     return cost1 + cost2 + cost3, dcost1 + dcost2 + tmp
 
 
 def fwd_model(x, svh, svv, theta):
     """Running the model forward to predict backscatter"""
-    n_obs = len(svv)
-    A_vv, B_vv, C_vv, A_vh, B_vh, C_vh = x[:6]
-    vsm = x[6 : (6 + n_obs)]
-    lai = x[(6 + n_obs) :]
-    sigma_vv, dvv = wcm_jac(A_vv, lai, B_vv, lai, C_vv, vsm, theta=theta)
-    sigma_vh, dvh = wcm_jac(A_vh, lai, B_vh, lai, C_vh, vsm, theta=theta)
+    n_obs = svh.shape[0]
+    A_vv, B_vv, A_vh, B_vh, ks = x[:5]
+    vsm = x[5 : (5 + n_obs)]
+    lai = x[(5 + n_obs) :]
+    sigma_vv, _ = wcm(A_vv, lai, B_vv, lai, vsm, ks, pol="VV", theta=theta)
+    sigma_vh, _ = wcm(A_vh, lai, B_vh, lai, vsm, ks, pol="HV", theta=theta)
     return sigma_vv, sigma_vh
-
-
-def reproject_data(
-    source_img,
-    target_img=None,
-    dstSRS=None,
-    srcSRS=None,
-    srcNodata=np.nan,
-    dstNodata=np.nan,
-    outputType=None,
-    output_format="MEM",
-    verbose=False,
-    xmin=None,
-    xmax=None,
-    ymin=None,
-    ymax=None,
-    xRes=None,
-    yRes=None,
-    xSize=None,
-    ySize=None,
-    resample=1,
-):
-
-    """
-    A method that uses a source and a target images to
-    reproject & clip the source image to match the extent,
-    projection and resolution of the target image.
-
-    """
-
-    outputType = gdal.GDT_Unknown if outputType is None else outputType
-    if srcNodata is None:
-        try:
-            srcNodata = " ".join(
-                [
-                    i.split("=")[1]
-                    for i in gdal.Info(source_img).split("\n")
-                    if " NoData" in i
-                ]
-            )
-        except RuntimeError:
-            srcNodata = None
-    # If the output type is intenger and destination nodata is nan
-    # set it to 0 to avoid warnings
-    if outputType <= 5 and np.isnan(dstNodata):
-        dstNodata = 0
-
-    if srcSRS is not None:
-        _srcSRS = osr.SpatialReference()
-        try:
-            _srcSRS.ImportFromEPSG(int(srcSRS.split(":")[1]))
-        except:
-            _srcSRS.ImportFromWkt(srcSRS)
-    else:
-        _srcSRS = None
-
-    if (target_img is None) & (dstSRS is None):
-        raise IOError(
-            "Projection should be specified ether from "
-            + "a file or a projection code."
-        )
-    elif target_img is not None:
-        try:
-            g = gdal.Open(target_img)
-        except RuntimeError:
-            g = target_img
-        geo_t = g.GetGeoTransform()
-        x_size, y_size = g.RasterXSize, g.RasterYSize
-
-        if xRes is None:
-            xRes = abs(geo_t[1])
-        if yRes is None:
-            yRes = abs(geo_t[5])
-
-        if xSize is not None:
-            x_size = 1.0 * xSize * xRes / abs(geo_t[1])
-        if ySize is not None:
-            y_size = 1.0 * ySize * yRes / abs(geo_t[5])
-
-        xmin, xmax = (
-            min(geo_t[0], geo_t[0] + x_size * geo_t[1]),
-            max(geo_t[0], geo_t[0] + x_size * geo_t[1]),
-        )
-        ymin, ymax = (
-            min(geo_t[3], geo_t[3] + y_size * geo_t[5]),
-            max(geo_t[3], geo_t[3] + y_size * geo_t[5]),
-        )
-        dstSRS = osr.SpatialReference()
-        raster_wkt = g.GetProjection()
-        dstSRS.ImportFromWkt(raster_wkt)
-        gg = gdal.Warp(
-            "",
-            source_img,
-            format=output_format,
-            outputBounds=[xmin, ymin, xmax, ymax],
-            dstNodata=dstNodata,
-            warpOptions=["NUM_THREADS=ALL_CPUS"],
-            xRes=xRes,
-            yRes=yRes,
-            dstSRS=dstSRS,
-            outputType=outputType,
-            srcNodata=srcNodata,
-            resampleAlg=resample,
-            srcSRS=_srcSRS,
-        )
-
-    else:
-        gg = gdal.Warp(
-            "",
-            source_img,
-            format=output_format,
-            outputBounds=[xmin, ymin, xmax, ymax],
-            xRes=xRes,
-            yRes=yRes,
-            dstSRS=dstSRS,
-            warpOptions=["NUM_THREADS=ALL_CPUS"],
-            copyMetadata=True,
-            outputType=outputType,
-            dstNodata=dstNodata,
-            srcNodata=srcNodata,
-            resampleAlg=resample,
-            srcSRS=_srcSRS,
-        )
-    if verbose:
-        print(
-            "There are %d bands in this file, use "
-            + "g.GetRasterBand(<band>) to avoid reading the whole file."
-            % gg.RasterCount
-        )
-    return gg
 
 
 ############ Some general functions for inversions #####
