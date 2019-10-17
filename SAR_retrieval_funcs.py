c8e1c6134e0f9063d6cdaedc2a9610e7e50ca0d7
Jose Gomez-Dans <jgomezdans@gmail.com>
2019-10-17 18:11:04 +0100

    New SAR functions

--- SAR_retrieval_funcs.py
+++ SAR_retrieval_funcs.py
@@ -9,57 +9,8 @@ import scipy.optimize
 import scipy.stats
 
 
-def oh2004(mv, ks, theta):
-    """Oh 2004 soil model as a function of volumetric soil moisture,
-    rms height and angle of incidence. 
-    Parameters
-    ----------
-    mv : float, ndarray
-        volumetric soil moisture m3/m3
-    ks : float
-        product of wavenumber and rms height
-        be aware that both need to have the same units
-    theta : float, ndarray
-        incidence angle [rad]
-    
-    Returns
-    -------
-    Backscatter (linear units) in HH, VV and HV polarisations
 
-    """
-    p1 = (2.0 * theta / np.pi) ** (0.35 * mv ** (-0.65))
-    p2 = np.exp(-0.4 * ks ** 1.4)
-    p = 1 - p1 * p2
-    # p = 1 - (2.*theta/np.pi)**(0.35*mv**(-0.65)) * np.exp(-0.4 * ks**1.4)
-    dp_dmv = 0.2275 * p1 * np.log(2.0 * theta / np.pi) * p2 / (mv ** 1.65)
-    dp_dks = 0.56 * p1 * np.exp(-0.4 * ks ** 1.4) * ks ** 0.4
-    q0 = 0.095 * (0.13 + np.sin(1.5 * theta)) ** 1.4
-    q = q0 * (1 - np.exp(-1.3 * ks ** 0.9))
-    dq_dmv = 0.0
-    dq_dks = q0 * (-1.17 * np.exp(-1.3 * ks ** 0.9) / ks ** 0.1)
-    a = 0.11 * mv ** 0.7 * np.cos(theta) ** 2.2
-    da_dmv = 0.7 * 0.11 * np.cos(theta) ** 2.2 / mv ** 0.3
-    da_dks = 0.0
-    b = 1 - np.exp(-0.32 * ks ** 1.8)
-    db_mv = 0.0
-    db_mks = 0.576 * np.exp(-0.32 * ks ** 1.8) * (ks ** 0.8)
-
-    hv = a * b
-    dhv_dmv = da_dmv * b
-    dhv_dks = db_mks * a
-    vv = a * b / q
-    dvv_dmv = (da_dmv * b * q) / (q * q)
-    dvv_dks = (db_mks * a * q + dq_dks * a * b) / (q * q)
-    hh = hv / q * p
-
-    return [
-        [hh, None],
-        [vv, np.array([dvv_dmv, dvv_dks])],
-        [hv, np.array([dhv_dmv, dhv_dks])],
-    ]
-
-
-def wcm(A, V1, B, V2, mv, ks, theta=23, pol="VV"):
+def wcm(A, V1, B, V2, mvs, R, theta=23, pol="VH"):
     """WCM model with Oh2004 model. The main
     assumption here is that we only consider first
     order effects. The vegetation backscatter contribution
@@ -70,28 +21,31 @@ def wcm(A, V1, B, V2, mv, ks, theta=23, pol="VV"):
     and frequency dependent). The soil backscatter is modelled as
     using an Oh2004 model.
     """
-    mu = np.cos(np.deg2rad(theta))
+    theta = np.deg2rad(theta)
+    mu = np.cos(theta)
+    
+    K = np.log10(0.11*(np.cos(theta)**2.2))
+    if pol.upper() == "VV":
+        K = K - np.log10(0.095*(0.13 + np.sin(1.5*theta))**1.4)
+    
     tau = np.exp(-2 * B * V2 / mu)
     veg = A * V1 * (1 - tau)
-    isel = ["HH", "VV", "HV"].index(pol.upper())
-    (sigma_soil, (dsoil_dmv, dsoil_dks)) = oh2004(mv, ks, np.deg2rad(theta))[
-        isel
-    ]
-    soil = tau * sigma_soil
+    sigma_soil = K + R + mvs
+    
 
     der_dA = V1 - V1 * tau
     der_dV1 = A - A * tau
     der_dB = (-2 * V2 / mu) * tau * (-A * V1 + sigma_soil)
     der_dV2 = (-2 * B / mu) * tau * (-A * V1 + sigma_soil)
-    der_dmv = tau * dsoil_dmv
-    der_dks = tau * dsoil_dks
-
+    der_dmvs = tau 
+    der_dR = tau 
+    
     return (
-        veg + soil,
-        np.array([der_dA, der_dV1, der_dB, der_dV2, der_dmv, der_dks]),
+        veg + tau*sigma_soil,
+        np.array([der_dA, der_dV1, der_dB, der_dV2, der_dmvs, der_dR]),
     )
-
-
+    
+    
 def cost_obs(x, svh, svv, theta, unc=0.5):
     """Cost function. Order of parameters is
     A_vv, B_vv, A_vh, B_vh, ks,
@@ -104,11 +58,13 @@ def cost_obs(x, svh, svv, theta, unc=0.5):
     although these are just labels!
     """
     n_obs = svh.shape[0]
-    A_vv, B_vv, A_vh, B_vh, ks = x[:5]
-    vsm = x[5 : (5 + n_obs)]
-    lai = x[(5 + n_obs) :]
-    sigma_vv, dvv = wcm(A_vv, lai, B_vv, lai, vsm, ks, pol="VV", theta=theta)
-    sigma_vh, dvh = wcm(A_vh, lai, B_vh, lai, vsm, ks, pol="HV", theta=theta)
+    A_vv, B_vv, R_vv, A_vh, B_vh, R_vh = x[:6]
+    vsm = x[6 : (6 + n_obs)]
+    lai = x[(6 + n_obs) :]
+    sigma_vv, dvv = wcm(A_vv, lai, B_vv, lai, vsm, R_vv, 
+                        pol="VV", theta=theta)
+    sigma_vh, dvh = wcm(A_vh, lai, B_vh, lai, vsm, R_vh,
+                        pol="HV", theta=theta)
     diff_vv = svv - sigma_vv
     diff_vh = svh - sigma_vh
     cost = 0.5 * (diff_vv ** 2 + diff_vh ** 2) / (unc ** 2)
@@ -118,9 +74,10 @@ def cost_obs(x, svh, svv, theta, unc=0.5):
                 [
                     np.sum(dvv[0] * diff_vv),  # A_vv
                     np.sum(dvv[2] * diff_vv),  # B_vv
+                    np.sum(dvv[5] * diff_vv),  # R_vv
                     np.sum(dvh[0] * diff_vh),  # A_vh
                     np.sum(dvh[2] * diff_vh),  # B_vh
-                    np.sum(dvv[5] * diff_vv + dvh[5] * diff_vh),  # ks
+                    np.sum(dvh[5] * diff_vh),  # R_vh
                 ]
             ),
             dvv[4] * diff_vv + dvh[4] * diff_vh,  # vsm
@@ -130,11 +87,12 @@ def cost_obs(x, svh, svv, theta, unc=0.5):
     return cost.sum(), -jac / (unc ** 2)
 
 
+
 def cost_prior(x, svh, svv, theta, prior_mean, prior_unc):
     """A Gaussian cost function prior. We assume no correlations
     between parameters, only mean and standard deviation.
     Cost function. Order of parameters is
-    A_vv, B_vv, C_vv, A_vh, B_vh, C_vh,
+    A_vv, B_vv, R_vv, A_vh, B_vh, R_vh,
     vsm_0, ..., vsm_N,
     LAI_0, ..., LAI_N
     
@@ -143,8 +101,9 @@ def cost_prior(x, svh, svv, theta, prior_mean, prior_unc):
     n_obs = len(svh)
     prior_cost = 0.5 * (prior_mean - x) ** 2 / prior_unc ** 2
     dprior_cost = -(prior_mean - x) / prior_unc ** 2
-    dprior_cost[:5] = 0.0
-    return (prior_cost[5:]).sum(), dprior_cost
+    dprior_cost[:6] = 0.0
+    return (prior_cost[6:]).sum(), dprior_cost
+
 
 
 def cost_smooth(x, gamma):
@@ -173,22 +132,27 @@ def cost_function(x, svh, svv, theta, gamma, prior_mean, prior_unc, unc=0.8):
     cost2, dcost2 = cost_prior(x, svh, svv, theta, prior_mean, prior_unc)
     # Smooth evolution of LAI
     n_obs = len(svv)
-    lai = x[(5 + n_obs) :]
+    lai = x[(6 + n_obs) :]
     cost3, dcost3 = cost_smooth(lai, gamma)
     tmp = np.zeros_like(dcost1)
-    tmp[(6 + n_obs) : -1] = dcost3
+    tmp[(7 + n_obs) : -1] = dcost3
     return cost1 + cost2 + cost3, dcost1 + dcost2 + tmp
 
 
 def fwd_model(x, svh, svv, theta):
     """Running the model forward to predict backscatter"""
     n_obs = svh.shape[0]
-    A_vv, B_vv, A_vh, B_vh, ks = x[:5]
-    vsm = x[5 : (5 + n_obs)]
-    lai = x[(5 + n_obs) :]
-    sigma_vv, _ = wcm(A_vv, lai, B_vv, lai, vsm, ks, pol="VV", theta=theta)
-    sigma_vh, _ = wcm(A_vh, lai, B_vh, lai, vsm, ks, pol="HV", theta=theta)
+    A_vv, B_vv, R_vv, A_vh, B_vh, R_vh = x[:6]
+    vsm = x[6 : (6 + n_obs)]
+    lai = x[(6 + n_obs) :]
+    sigma_vv, _ = wcm(A_vv, lai, B_vv, lai, vsm, R_vv, 
+                        pol="VV", theta=theta)
+    sigma_vh, _ = wcm(A_vh, lai, B_vh, lai, vsm, R_vh,
+                        pol="HV", theta=theta)
+
     return sigma_vv, sigma_vh
+
+
 
 
 ############ Some general functions for inversions #####
@@ -249,7 +213,7 @@ def do_plots(field, retval, svv, svh, theta, doy, df, s2_lai):
     )
     axs[1].axhspan(-0.5, 0.5, color="0.8")
     axs[1].legend(loc="best")
-    l1 = axs[2].plot(doy, retval.x[5 : (n_obs + 5)], "r-o", label="sigma soil")
+    l1 = axs[2].plot(doy, retval.x[6 : (n_obs + 6)], "r-o", label="sigma soil")
     axx = axs[2].twinx()
     l2 = axx.plot(
         df[f"doy_{field:s}"], df[f"SM_{field:s}"], "s-g", label="Sigma SM"
@@ -263,7 +227,7 @@ def do_plots(field, retval, svv, svh, theta, doy, df, s2_lai):
     l2 = axs[3].plot(
         df[f"doy_{field:s}"], df[f"LAI_{field:s}"], "o", label="In situ"
     )
-    l3 = axs[3].plot(doy, retval.x[(n_obs + 5) :], label="Analysis")
+    l3 = axs[3].plot(doy, retval.x[(n_obs + 6) :], label="Analysis")
     axs[0].set_ylabel("Backscatter [dB]")
     axs[1].set_ylabel("Residual [dB]")
     axs[3].set_ylabel("Leaf area index $[m^2\,m^{-2}]$")
@@ -272,20 +236,25 @@ def do_plots(field, retval, svv, svh, theta, doy, df, s2_lai):
     axs[3].legend(legends, labels, loc="best")
 
 
-def invert_field(svv, svh, theta, prior_mean, prior_sd, gamma, s2_lai, ks0=4):
+def invert_field(svv, svh, theta, prior_mean, prior_sd, gamma, s2_lai):
     n_obs = len(svv)
-    xvv = np.array([1, 0.5])
-    xvh = np.array([1, 0.5, ks0])
-    sm0 = prior_mean[5 : (5 + n_obs)]
+    xvv = np.array([1, 0.5, 0.])
+    xvh = np.array([1, 0.5, 0.])
+    sm0 = prior_mean[6 : (6 + n_obs)]
     # In reality, this should come from a sensible prior mean, but for the
     # time being...
     x0 = np.concatenate([xvv, xvh, sm0, s2_lai])
 
     # Put some parameter bounds so we don't end up with crazy numbers
     bounds = (
-        [[None, None]] * 4
-        + [0.1, 20]
-        + [[0, 0.5]] * s2_lai.shape[0]
+        [
+        [None, None],
+        [None, None],
+        [-4, 0],
+        [None, None],
+        [None, None],
+        [-4, 0]]       
+        + [[0.05, 0.4]] * s2_lai.shape[0]
         + [[0, 8]] * s2_lai.shape[0]
     )
     # Minimise the log-posterior
@@ -303,3 +272,70 @@ def invert_field(svv, svh, theta, prior_mean, prior_sd, gamma, s2_lai, ks0=4):
     )
     print(f"Final cost {retval.fun:g}")
     return retval
+
+
+
+
+    
+    
+def test_wcm_vv():
+    """Test the WCM mode forward operation. Assume R_pq=0 and mvs=0"""
+    A, B, V1, V2, mvs = -12,  0.05, np.ones(2)*4, np.ones(2)*4, np.zeros(2)
+    R = np.zeros(2)
+    retval_vv = wcm(A, V1, B, V2, mvs, R, theta=36, pol="VV")
+
+    expected = np.array([-18.7851694, -18.7851694])
+    assert np.allclose( retval_vv[0], expected)
+
+    
+def test_wcm_vh():
+    """Test the WCM mode forward operation. Assume R_pq=0 and mvs=0"""
+    A, B, V1, V2, mvs = -5,  0.1, np.ones(2)*4, np.ones(2)*4, np.zeros(2)
+    R = np.zeros(2)
+    retval_vh = wcm(A, V1, B, V2, mvs, R, theta=36, pol="VH")
+    expected = np.array([-12.99188003, -12.99188003])
+    assert np.allclose( retval_vh[0], expected)
+
+
+def test_cost_wcm():
+    sigma_vv = np.array([-18.7851694, -18.7851694])
+    sigma_vh = np.array([-12.99188003, -12.99188003])
+    theta = np.ones(2)*36.
+    A_vv, B_vv, R_vv, A_vh, B_vh, R_vh = (-12, 0.05, 0., -5, 0.1, 0)
+    
+
+    x = np.concatenate([np.array([A_vv, B_vv, R_vv, A_vh, B_vh, R_vh]),
+                       np.ones(2)*0.,np.ones(2)*4.])
+    retval = cost_obs(x, sigma_vh, sigma_vv, theta, unc=0.5)
+    assert np.allclose(retval[0], 0., atol=1e-3)
+    
+
+def test_cost_wcm_jac0():
+    sigma_vv = np.array([-18.7851694, -18.7851694])
+    sigma_vh = np.array([-12.99188003, -12.99188003])
+    theta = np.ones(2)*36.
+    A_vv, B_vv, R_vv, A_vh, B_vh, R_vh = (-12, 0.05, 0., -5, 0.1, 0)
+    
+
+    x = np.concatenate([np.array([A_vv, B_vv, R_vv, A_vh, B_vh, R_vh]),
+                       np.ones(2)*0.,np.ones(2)*4.])
+    retval = cost_obs(x, sigma_vh, sigma_vv, theta, unc=0.5)
+    assert np.allclose(retval[1], np.zeros_like(retval[1]), atol=1e-3)
+    
+
+
+def test_cost_wcm_jac1():
+    sigma_vv = np.array([-18.7851694, -18.7851694])
+    sigma_vh = np.array([-12.99188003, -12.99188003])
+    theta = np.ones(2)*36.
+    A_vv, B_vv, R_vv, A_vh, B_vh, R_vh = (-10, 0.06, 0.1, -4, 0.11, 0.2)
+    
+    func = lambda xx: cost_obs(xx, sigma_vh, sigma_vv, theta, unc=0.5)[0]
+
+    x = np.concatenate([np.array([A_vv, B_vv, R_vv, A_vh, B_vh, R_vh]),
+                       np.ones(2)*0.,np.ones(2)*4.])
+    approx_jac = scipy.optimize.approx_fprime(x, func, 1e-9)
+    true_jac = cost_obs(x, sigma_vh, sigma_vv, theta, unc=0.5)[1]
+
+    assert np.allclose(approx_jac, true_jac, atol=0.1)
+    
